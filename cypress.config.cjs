edbfbe779afad2a3e7597ac87725f5aa5be7b501
AJ Kozlak II <ajkozlak2@icloud.com>
2025-06-13 16:56:50 -0500

    major push

---
+++ cypress.config.cjs
@@ -0,0 +1,18 @@
+// cypress.config.cjs
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/component.ts',
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.ts',
+  },
+});
